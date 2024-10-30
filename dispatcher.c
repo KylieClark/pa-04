4f16ed25510bc3ed1ed68e813d51950663ebd5ef
Kylie Clark <clar24ka@dukes.jmu.edu>
2024-10-29 22:16:29 -0400

    setup

--- dispatcher.c
+++ dispatcher.c
@@ -21,24 +21,26 @@ Submitted on:
 //--------------------------------------------------------------------------
 int main( int argc , char *argv[] )
 {
-    pid_t  amalPID , basimPID ; 
-    int    AtoB_ctrl[2] , AtoB_data[2] , AtoKDC_data[2] , KDCtoA_ctrl[2] ;  // Amal to Basim control and data pipes
+    pid_t  amalPID , basimPID , kdcPID ; 
+    int    AtoB_ctrl[2] , AtoB_data[2] , KDCtoA_ctrl[2] , KDCtoA_data[2] ;  // Amal to Basim control and data pipes
     char   arg1[20] , arg2[20] , arg3[20] , arg4[20];
     
     Pipe( AtoB_ctrl ) ;  // create pipe for Amal-to-Basim control
     Pipe( AtoB_data ) ;  // create pipe for Amal-to-Basim data
-    Pipe( AtoKDC_data);  // create pipe for Amal-to-KDC data
-    Pipe( KDCtoA_ctrl);  // create pipe for KDC-to-Amal data
+    Pipe( KDCtoA_ctrl);  // create pipe for Amal-to-KDC data
+    Pipe( KDCtoA_data);  // create pipe for KDC-to-Amal data
 
     printf("\tDispatcher started and created these pipes\n") ;
     printf("\tAmal-to-Basim control pipe: read=%d  write=%d\n", AtoB_ctrl[ READ_END ] , AtoB_ctrl[ WRITE_END ] ) ;
     printf("\tAmal-to-Basim data    pipe: read=%d  write=%d\n", AtoB_data[ READ_END ] , AtoB_data[ WRITE_END ] ) ;
-    printf("\tAmal-to-KDC data    pipe: read=%d  write=%d\n", AtoKDC_data[ READ_END ] , AtoKDC_data[ WRITE_END ] ) ;
-    printf("\tKDC-to-Amal data    pipe: read=%d  write=%d\n", KDCtoA_ctrl[ READ_END ] , KDCtoA_ctrl[ WRITE_END ] ) ;
+    printf("\tKDC-to-Amal control   pipe: read=%d  write=%d\n", KDCtoA_ctrl[ READ_END ] , KDCtoA_ctrl[ WRITE_END ] ) ;
+    printf("\tKDC-to-Amal data      pipe: read=%d  write=%d\n", KDCtoA_data[ READ_END ] , KDCtoA_data[ WRITE_END ] ) ;
 
 
     // Create both child processes:
     amalPID = Fork() ;
+    basimPID = Fork() ;
+    kdcPID = Fork() ;
     if ( amalPID == 0 )
     {    
         // This is the Amal process.
@@ -58,49 +60,46 @@ int main( int argc , char *argv[] )
         perror("ERROR starting Amal" );
         exit(-1) ;      
     } 
+    else if ( basimPID == 0 )
+    {   // This is still the Dispatcher process  
+        // This is the Basim process
+        // Basim will not use these ends of the pipes, decrement their 'count'
+        close( AtoB_ctrl[ WRITE_END ] ) ;
+        close( AtoB_data[ WRITE_END ] ) ;
+        
+        // Prepare the file descriptors as args to Basim
+        snprintf( arg1 , 20 , "%d" , AtoB_ctrl[ READ_END ] ) ;
+        snprintf( arg2 , 20 , "%d" , AtoB_data[ READ_END ] ) ;
+
+        char * cmnd = "./basim/basim" ;
+        execlp( cmnd , "Basim" , arg1 , arg2 , NULL );
+
+        // the above execlp() only returns if an error occurs
+        perror("ERROR starting Basim" ) ;
+        exit(-1) ;
+    }
+    // make kdc here!!!!!!!!
     else
-    {    // This is still the Dispatcher process 
-        basimPID = Fork() ;
-        if ( basimPID == 0 )
-        {  
-            // This is the Basim process
-            // Basim will not use these ends of the pipes, decrement their 'count'
-            close( AtoB_ctrl[ WRITE_END ] ) ;
-            close( AtoB_data[ WRITE_END ] ) ;
-            
-            // Prepare the file descriptors as args to Basim
-            snprintf( arg1 , 20 , "%d" , AtoB_ctrl[ READ_END ] ) ;
-            snprintf( arg2 , 20 , "%d" , AtoB_data[ READ_END ] ) ;
+    {   // This is still the parent Dispatcher  process
+        // close all ends of the pipes so that their 'count' is decremented
+        close( AtoB_ctrl[ WRITE_END ] ); 
+        close( AtoB_ctrl[ READ_END  ]  );   
+        close( AtoB_data[ WRITE_END ] ); 
+        close( AtoB_data[ READ_END  ]  );   
 
-            char * cmnd = "./basim/basim" ;
-            execlp( cmnd , "Basim" , arg1 , arg2 , NULL );
+        printf("\n\tDispatcher is now waiting for Amal to terminate\n") ;
+        int  exitStatus ;
+        waitpid( amalPID , &exitStatus , 0 ) ;
+        printf("\n\tAmal terminated ... "  ) ;
+        if (  WIFEXITED( exitStatus ) )
+                printf(" with status =%d\n" , WEXITSTATUS(exitStatus ) ) ;
 
-            // the above execlp() only returns if an error occurs
-            perror("ERROR starting Basim" ) ;
-            exit(-1) ;
-        }
-        else
-        {   // This is still the parent Dispatcher  process
-            // close all ends of the pipes so that their 'count' is decremented
-            close( AtoB_ctrl[ WRITE_END ] ); 
-            close( AtoB_ctrl[ READ_END  ]  );   
-            close( AtoB_data[ WRITE_END ] ); 
-            close( AtoB_data[ READ_END  ]  );   
-
-            printf("\n\tDispatcher is now waiting for Amal to terminate\n") ;
-			int  exitStatus ;
-            waitpid( amalPID , &exitStatus , 0 ) ;
-            printf("\n\tAmal terminated ... "  ) ;
-			if (  WIFEXITED( exitStatus ) )
-                    printf(" with status =%d\n" , WEXITSTATUS(exitStatus ) ) ;
-
-            printf("\n\tDispatcher is now waiting for Basim to terminate\n") ;
-            waitpid( basimPID , &exitStatus , 0 ) ;
-            printf("\n\tBasim terminated ... " ) ;
-			if (  WIFEXITED( exitStatus ) )
-                    printf(" with status =%d\n" , WEXITSTATUS(exitStatus ) ) ;
-     
-        }
+        printf("\n\tDispatcher is now waiting for Basim to terminate\n") ;
+        waitpid( basimPID , &exitStatus , 0 ) ;
+        printf("\n\tBasim terminated ... " ) ;
+        if (  WIFEXITED( exitStatus ) )
+        printf(" with status =%d\n" , WEXITSTATUS(exitStatus ) ) ;
+    
     }
 }
 
